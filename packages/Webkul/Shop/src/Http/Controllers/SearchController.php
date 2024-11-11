b3bd327d00430c924a9d47e189248e1289e0af36
idist-hn <HanhHanh2508@>
2024-11-11 10:02:31 +0700

    fix search all page

--- packages/Webkul/Shop/src/Http/Controllers/SearchController.php
+++ packages/Webkul/Shop/src/Http/Controllers/SearchController.php
@@ -24,8 +24,9 @@ class SearchController extends Controller
      */
     public function index()
     {
+
         $this->validate(request(), [
-            'query' => ['required', 'string', 'regex:/^[^\\\\]+$/u'],
+            'query' => [ 'string', 'regex:/^[^\\\\]+$/u'],
         ]);
 
         $searchTerm = $this->searchTermRepository->findOneWhere([
